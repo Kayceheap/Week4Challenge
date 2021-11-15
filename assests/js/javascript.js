ba3870d45961a26a184acac5599642440bc08c4a
Kayce Heap <kayce.heap@gmail.com>
2021-11-14 19:57:36 -0700

    started html, js and css code. started working off the html body and adding dynamaic html through javascript

---
+++ assests/js/javascript.js
@@ -0,0 +1,75 @@
+var questions = [
+    {
+        question: "What is the main character's name?",
+        answer: "Kevin",
+        options: ["Steve", "Carl", "Santa", "Kevin"]
+    },
+    {
+        question: "What pet does Buzz own?",
+        answer: "Tarantula",
+        options: ["Tarantula", "Snake", "Dog", "Cat"]
+    },
+    {
+        question: "What does Kevin steal from the drug store?",
+        answer: "Toothbrush",
+        options: ["Candy", "Bandaid", "Toothbrush", "Baseball Cards"]
+    },
+];
+
+var currentQuestionIndex = 0;
+
+
+
+
+
+var answerClicked = function (event) {
+    if (event.target.textContent === questions[currentQuestionIndex].answer) {
+        window.alert("correct!")
+    }
+    else {
+        window.alert("wrong")
+    }
+
+}
+var questionList = document.querySelector("#questionList");
+questionList.addEventListener("click", answerClicked);
+
+var displayQuestion = function (question) {
+    var questionParent = document.querySelector("#questionList");
+    var questionText = document.createElement("div");
+    questionText.textContent = question.question;
+    questionParent.appendChild(questionText);
+
+    var currentOptions = question.options;
+    for (var i = 0; i < currentOptions.length; i++) {
+        var optionBox = document.createElement("div");
+        optionBox.textContent = currentOptions[i];
+        questionParent.appendChild(optionBox);
+    }
+}
+
+
+
+var timeLeft = 60;
+
+var startButton = document.querySelector("#quizStart");
+
+var updateTimer = function () {
+    var timerStart = document.querySelector("#timer");
+    timerStart.textContent = timeLeft;
+    timeLeft--;
+}
+
+var startQuiz = function (event) {
+
+
+
+    var interval = setInterval(updateTimer, 1000);
+
+    // Display first question
+
+    var currentQuestion = questions[0];
+    displayQuestion(currentQuestion);
+}
+startButton.addEventListener("click", startQuiz);
+
